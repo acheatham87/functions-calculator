73fbb02a82d57aa174c1879f4d521eb72ed72339
acheatham87 <acheatham87@gmail.com>
2022-02-17 11:18:21 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+const add = (num1, num2) => {
+    const sum = num1 + num2
+    return sum
+}
+
+// let result = add(15, 12)
+// console.log(result)
+
+const divide = (num1, num2) => {
+    const remainder = num1 / num2
+    return remainder
+}
+// let result = divide(64, 8)
+// console.log(result)
+
+const multiply = (num1, num2) => {
+    const product = num1 * num2
+    return product
+}
+// let result = multiply(15, 5)
+// console.log(result)
+
+const square = (num1) => {
+    const squared = num1 * num1
+    return squared
+}
+let result = square(6)
+console.log(result)
